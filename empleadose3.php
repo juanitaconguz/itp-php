885878de97ca065844f2d012fbd48d8d4ff76428
Your Name <you@example.com>
2014-06-11 14:07:33 -0500

    lear

---
+++ empleadose3.php
@@ -0,0 +1,23 @@
+<?php
+$empleados =
+[
+    ['nombre' => 'Juanita bonita',
+     'sueldo' =>  '10000',
+     'depto' => 'Tintes'
+    ], 	
+    ['nombre' => 'Carlos Luna',
+     'sueldo' =>  '5000',
+     'depto' => 'Tintes'
+    ], 	
+    ['nombre' => 'Antonio Sol',
+     'sueldo' =>  '100000'
+     'depto' => 'Armado'
+    ], 	
+    ['nombre' => 'Alberto Dias',
+     'sueldo' =>  '8000'
+     'depto' => 'Armado'
+    ], 	
+
+];
+return $empleados;
+?>
---
+++ nomina.php
@@ -0,0 +1,20 @@
+<?php
+//lo pase a nomina.php que si corre
+
+/*
+para ael proceso de pago de un empleado se debe ingresar la cantidad ganada.
+ Al ingresar la cantidad se tiene que calcular los impuestos tales como IVA (15%) 
+e ISR (10%) mostrando la cantidad de los impuestos
+y el sueldo total para el empleado, DE VARIOS EMPLEADOS
+*/
+
+$empleados = require 'empleados.php';
+
+echo "nombre     --   sueldo\n";
+foreach($empleados as $empleado) {
+   $empleado['sueldo'] * .10;
+   $empleado['sueldo'] * .15;
+   echo "\n{$empleado['nombre']}         --    {$empleado['sueldo']}";
+}
+
+?>
